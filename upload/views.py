6a9ab5af3283aaa479e4d3c5b65d1d4811ff7280
jilinxie <jilinxie1988@gmail.com>
2014-10-30 15:36:22 +0800

    convert file name to unique seq

--- upload/views.py
+++ upload/views.py
@@ -5,6 +5,8 @@ import json
 import qiniu.conf
 import qiniu.io
 import qiniu.rs
+import os
+import uuid
 
 class Qiniu_Setting(object):
     qiniu.conf.ACCESS_KEY = '0V3pdbcLhNOuUTs71Q-ynVTx3CM8DJZ0Va5Y9BiF'
@@ -28,7 +30,8 @@ def upload(data, dsc_key):
 def upload_view(request):
     if request.method == 'POST':
         file_obj = request.FILES['file']
-        pictureLink = upload(file_obj.read(), file_obj.name)
+        uname = uuid.uuid4().hex+'.'+ os.path.splitext(file_obj.name)[1]
+        pictureLink = upload(file_obj.read(), uname)
         resp =  HttpResponse(json.dumps({'key': pictureLink, 'ret_code':0}), content_type='text/plain')#'application/json')
 
         return resp
